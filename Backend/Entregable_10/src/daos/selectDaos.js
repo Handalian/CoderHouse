93d1ad0f7e55818cb638bc63660965e70e36f4d6
Handalian <73723793+Handalian@users.noreply.github.com>
2022-10-10 21:24:41 -0300

    Delete Backend/Entregable_10/src directory

--- Backend/Entregable_10/src/daos/selectDaos.js
+++
@@ -1,5 +0,0 @@
-/*Noo Funciona*/
-
-const selector = 'Archivo';
-export let classCarts = await import(`./carritos/ClassCarrito${selector}.js`)
-export let classProductos = await import(`./productos/ClassProductos${selector}.js`)
